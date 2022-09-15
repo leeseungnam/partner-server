a74f1260f53bab3fd6d658df199664b2613afdbb
chals <chals@wrightbrothers.kr>
2022-09-15 23:36:48 +0900

    Valid 어노테이션 추가

--- src/main/java/kr/wrightbrothers/apps/product/dto/DeliveryDto.java
+++ src/main/java/kr/wrightbrothers/apps/product/dto/DeliveryDto.java
@@ -5,6 +5,11 @@ import lombok.*;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.jackson.Jacksonized;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class DeliveryDto {
 
     @Data
@@ -13,18 +18,29 @@ public class DeliveryDto {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class Delivery {
+        @NotBlank
         private String deliveryType;
+        @NotBlank
         private String deliveryBundleFlag;
+        @NotBlank
         private String chargeType;
+        @NotNull
+        @Min(value = 100, message = "기본 배송비")
+        @Max(value = 100000000, message = "기본 배송비")
         private Integer chargeBase;
         private Long termsFreeCharge;
+        @NotBlank
         private String paymentType;
+        @NotBlank
         private String surchargeFlag;
         private String areaCode;
         private Integer surchargeJejudo;
         private Integer surchargeIsolated;
+        @NotBlank
         private String unstoringAddress;
+        @NotBlank
         private String returnAddress;
+        @NotNull
         private Integer returnCharge;
         private String returnDeliveryCompanyCode;
     }
--- src/main/java/kr/wrightbrothers/apps/product/dto/SellInfoDto.java
+++ src/main/java/kr/wrightbrothers/apps/product/dto/SellInfoDto.java
@@ -5,6 +5,11 @@ import lombok.*;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.jackson.Jacksonized;
 
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class SellInfoDto {
 
     @Data
@@ -13,13 +18,19 @@ public class SellInfoDto {
     @AllArgsConstructor
     @NoArgsConstructor
     public static class SellInfo {
+        @NotNull(message = "상품금액")
         private Long productAmount;
+        @NotBlank(message = "할인여부")
         private String discountFlag;
         private String discountType;
         private String discountAmount;
+        @NotBlank(message = "전시상태")
         private String displayFlag;
+        @NotNull(message = "판매가")
         private Long finalSellAmount;
+        @NotBlank(message = "상품상태")
         private String productStatusCode;
+        @Min(value = 1, message = "재고")
         private int productStockQty;
     }
 
