3726e58aafd88007a25577526a7c4c1d17791dcd
chals <chals@wrightbrothers.kr>
2022-09-22 18:34:03 +0900

    주소록 트랜잭션 transactionManager 옵션 추가

--- src/test/java/kr/wrightbrothers/apps/AddressControllerTest.java
+++ src/test/java/kr/wrightbrothers/apps/AddressControllerTest.java
@@ -52,7 +52,7 @@ class AddressControllerTest extends BaseControllerTests {
     }
 
     @Test
-    @Transactional
+    @Transactional(transactionManager = PartnerKey.WBDataBase.TransactionManager.Default)
     @DisplayName("주소록 목록 조회")
     void findAddressList() throws Exception {
         // 주소 목록 조회 API
