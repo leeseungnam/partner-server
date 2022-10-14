6dfb5730a663c92c5746709e7e863c5688e6a277
chals <chals@wrightbrothers.kr>
2022-10-14 17:22:12 +0900

    메일발송 테스트

--- src/test/java/kr/wrightbrothers/util/AwsTest.java
+++ src/test/java/kr/wrightbrothers/util/AwsTest.java
@@ -2,6 +2,8 @@ package kr.wrightbrothers.util;
 
 import kr.wrightbrothers.BaseControllerTests;
 import kr.wrightbrothers.apps.common.util.AwsSesUtil;
+import kr.wrightbrothers.apps.email.dto.SingleEmailDto;
+import kr.wrightbrothers.apps.email.service.EmailService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,20 +17,30 @@ class AwsTest extends BaseControllerTests {
 
     @Autowired
     private AwsSesUtil awsSesUtil;
+    @Autowired
+    private EmailService emailService;
 
+    @Test
     @DisplayName("SES 메일발송 테스트")
     void SesSendTest() {
 
-        String subject = "메일발송 테스트";
+        emailService.singleSendEmail(SingleEmailDto.ReqBody.builder()
+                        .authCode("3333")
+                        .emailType("1")
+                        .userId("chals@wrightbrothers.kr")
+                .build());
 
-        Context context = new Context();
-        context.setVariable("code", 7777);
 
-        awsSesUtil.singleSend(
-                subject,
-                "userMailAuth",
-                context,
-                "chals@wrightbrothers.kr");
+//        String subject = "메일발송 테스트";
+//
+//        Context context = new Context();
+//        context.setVariable("code", 7777);
+//
+//        awsSesUtil.singleSend(
+//                subject,
+//                "userMailAuth",
+//                context,
+//                "chals@wrightbrothers.kr");
     }
 
 }
