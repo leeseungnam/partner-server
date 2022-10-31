f168dda0eb4abc42d6a49e423e9fe594b2acb518
snlee <snlee@wrightbrothers.kr>
2022-11-01 08:55:26 +0900

    constants 변경 - user

--- src/main/java/kr/wrightbrothers/apps/sign/service/WBUserDetailService.java
+++ src/main/java/kr/wrightbrothers/apps/sign/service/WBUserDetailService.java
@@ -1,43 +1,35 @@
 package kr.wrightbrothers.apps.sign.service;
 
-import kr.wrightbrothers.apps.common.util.PartnerKey;
+import kr.wrightbrothers.apps.common.constants.User;
 import kr.wrightbrothers.apps.sign.dto.UserDetailDto;
 import kr.wrightbrothers.apps.sign.dto.UserPrincipal;
-import kr.wrightbrothers.framework.lang.WBBusinessException;
-import kr.wrightbrothers.framework.support.dao.WBCommonDao;
 import lombok.RequiredArgsConstructor;
-import org.apache.ibatis.javassist.NotFoundException;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
-import software.amazon.awssdk.services.connect.model.UserNotFoundException;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class WBUserDetailService implements UserDetailsService {
 
     private final SignService signService;
+
+    private final String messagePrefix = "api.message.";
+    private final MessageSourceAccessor messageSourceAccessor;
 
     @Override
     public UserDetails loadUserByUsername(final String userId) throws UsernameNotFoundException {
 
         UserDetailDto user = signService.loadUserByUsername(userId);
 
-        if(ObjectUtils.isEmpty(user)) throw new UsernameNotFoundException("입력하신 이메일주소 및 비밀번호를 확인해주세요.");
+        if(ObjectUtils.isEmpty(user)) throw new UsernameNotFoundException(messageSourceAccessor.getMessage(messagePrefix+"user.empty"));
 
-        if(PartnerKey.Code.User.Status.DROP_REQUEST.equals(user.getUserStatusCode())) {
-            throw new UsernameNotFoundException("탈퇴 진행 중인 사용자 입니다.");
-        } else if(PartnerKey.Code.User.Status.DROP_COMPLETE.equals(user.getUserStatusCode())) {
-            throw new UsernameNotFoundException("탈퇴한 파트너 센터 아이디 입니다.");
-        }
         return UserPrincipal.createUser(user);
     }
 }
