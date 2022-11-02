d339a371f8ccab2d96119bea90bb77f577467344
chals <chals@wrightbrothers.kr>
2022-11-02 14:29:20 +0900

    SQS 상세 설정 AWS Config 추가

--- src/main/java/kr/wrightbrothers/apps/common/config/AWSConfigure.java
+++ src/main/java/kr/wrightbrothers/apps/common/config/AWSConfigure.java
@@ -3,11 +3,15 @@ package kr.wrightbrothers.apps.common.config;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
 import kr.wrightbrothers.framework.util.WBAwsSns;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.aws.messaging.config.SimpleMessageListenerContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.services.ses.SesClient;
@@ -32,7 +36,7 @@ public class AWSConfigure {
     @Value("${cloud.aws.region.static}")
     private String region;
     @Value("${cloud.aws.sqs.auto}")
-    private Boolean use;
+    private Boolean isAuto;
 
     @Bean
     public SesClient amazonSimpleEmailServiceAsync() {
@@ -56,4 +60,25 @@ public class AWSConfigure {
         );
     }
 
+    @Bean
+    public SimpleMessageListenerContainerFactory simpleMessageListenerContainerFactory(AmazonSQSAsync amazonSQSAsync) {
+        SimpleMessageListenerContainerFactory factory = new SimpleMessageListenerContainerFactory();
+        factory.setAmazonSqs(amazonSQSAsync);
+        factory.setAutoStartup(isAuto);
+        factory.setWaitTimeOut(1);
+        factory.setMaxNumberOfMessages(1);
+        factory.setTaskExecutor(createDefaultTaskExecutor());
+
+        return factory;
+    }
+
+    protected AsyncTaskExecutor createDefaultTaskExecutor() {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setThreadNamePrefix("SQSExecutor - ");
+        threadPoolTaskExecutor.setCorePoolSize(100);
+        threadPoolTaskExecutor.setMaxPoolSize(100);
+        threadPoolTaskExecutor.setQueueCapacity(2);
+        threadPoolTaskExecutor.afterPropertiesSet();
+        return threadPoolTaskExecutor;
+    }
 }
