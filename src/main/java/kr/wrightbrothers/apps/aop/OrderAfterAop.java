b0f44a8f5e9d52f11dd8ead6a5873194323066ca
chals <chals@wrightbrothers.kr>
2022-11-18 00:10:52 +0900

    주문 수정 시 히스토리 전달

---
+++ src/main/java/kr/wrightbrothers/apps/aop/OrderAfterAop.java
@@ -0,0 +1,53 @@
+package kr.wrightbrothers.apps.aop;
+
+import kr.wrightbrothers.apps.common.type.DocumentSNS;
+import kr.wrightbrothers.apps.common.util.PartnerKey;
+import kr.wrightbrothers.apps.queue.HistoryQueue;
+import kr.wrightbrothers.framework.util.JsonUtil;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.json.JSONObject;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Slf4j
+@Aspect
+@Configuration
+@RequiredArgsConstructor
+public class OrderAfterAop {
+
+    private final HistoryQueue historyQueue;
+
+    @AfterReturning(value =
+            "execution(* kr.wrightbrothers.apps.order.service.*Service.update*(..)))"
+    )
+    public void sendHistoryQueue(JoinPoint joinPoint) throws Exception {
+        JSONObject object = new JSONObject(JsonUtil.ToString(Arrays.stream(joinPoint.getArgs()).findFirst().orElseThrow()));
+
+        if (!object.has("orderNo")) return;
+
+        log.debug("Order History Send SNS. Order No::{},", object.getString("orderNo"));
+        historyQueue.sendToAdmin(
+                DocumentSNS.UPDATE_ORDER,
+                HistoryMessages.builder()
+                        .appKey(object.getString("orderNo"))
+                        .appNm("파트너주문")
+                        .build(),
+                PartnerKey.TransactionType.Update
+        );
+    }
+
+    @Getter
+    @Builder
+    static class HistoryMessages {
+        private String appKey;
+        private String appNm;
+        private Object data;
+    }
+}
---
+++ src/main/java/kr/wrightbrothers/apps/queue/HistoryQueue.java
@@ -0,0 +1,48 @@
+package kr.wrightbrothers.apps.queue;
+
+import kr.wrightbrothers.apps.common.type.DocumentSNS;
+import kr.wrightbrothers.framework.support.WBSQS;
+import kr.wrightbrothers.framework.support.dto.WBSnsDTO;
+import kr.wrightbrothers.framework.util.WBAwsSns;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class HistoryQueue extends WBSQS {
+
+    private final WBAwsSns sender;
+    @Value("${cloud.aws.sns.history}")
+    private String topic;
+
+    /**
+     * 입점몰 API -> ADMIN 2.0 API
+     * AWS SNS Message Queue 발송 처리
+     *
+     * @param documentSNS 문서 명
+     * @param messages 전송 메시지
+     * @param transactionType 전송 타입
+     */
+    public void sendToAdmin(DocumentSNS documentSNS,
+                            Object messages,
+                            String transactionType) {
+        try {
+            // AWS SNS 전송
+            sender.send(
+                    topic,
+                    WBSnsDTO.Header.builder()
+                            .docuNm(documentSNS.getName())
+                            .trsctnTp(transactionType)
+                            .build(),
+                    messages
+            );
+        } catch (Exception e) {
+            log.error("History SNS Sender Error. {}", ExceptionUtils.getStackTrace(e));
+        }
+    }
+
+}
