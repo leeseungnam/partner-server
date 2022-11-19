15a937553a022d68a65a047c478cd36f5b4d5ba8
snlee <snlee@wrightbrothers.kr>
2022-11-19 19:57:05 +0900

    build profile

--- deploy.sh
+++ deploy.sh
@@ -20,4 +20,4 @@ else
 fi
 
 echo "> $APPLICATION_JAR 배포"
-nohup java -jar -Dspring.profiles.active=dev -Duser.timezone=Asia/Seoul $APPLICATION_JAR > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar -Duser.timezone=Asia/Seoul $APPLICATION_JAR > /dev/null 2> /dev/null < /dev/null &
