88b83b6b6abf5fdb1496bd609b0129039bc70f0c
chals <chals@wrightbrothers.kr>
2022-11-20 23:36:36 +0900

    배송 엑셀 어노테이션 누락 추가

--- src/main/java/kr/wrightbrothers/apps/order/dto/DeliveryExcelDto.java
+++ src/main/java/kr/wrightbrothers/apps/order/dto/DeliveryExcelDto.java
@@ -1,5 +1,6 @@
 package kr.wrightbrothers.apps.order.dto;
 
+import kr.wrightbrothers.apps.common.annotation.ExcelBody;
 import kr.wrightbrothers.apps.common.type.DeliveryStatusCode;
 import kr.wrightbrothers.apps.common.type.DeliveryType;
 import kr.wrightbrothers.apps.common.util.MaskingUtil;
@@ -23,28 +24,51 @@ public class DeliveryExcelDto {
     public static class Response {
         private int orderProductCount;
         private int productCount;
+        @ExcelBody(colIndex = 1)
         private String paymentDay;
+        @ExcelBody(colIndex = 2)
         private String orderNo;
+        @ExcelBody(colIndex = 3)
         private String orderDay;
+        @ExcelBody(colIndex = 4)
         private String orderName;
+        @ExcelBody(colIndex = 5)
         private String productCode;
+        @ExcelBody(colIndex = 6)
         private String productName;
+        @ExcelBody(colIndex = 7)
         private String productOption;
+        @ExcelBody(colIndex = 8)
         private Integer productQty;
+        @ExcelBody(colIndex = 9)
         private Long productSellAmount;
+        @ExcelBody(colIndex = 10)
         private Long productAmount;
+        @ExcelBody(colIndex = 11)
         private Long productDeliveryChargeAmount;
+        @ExcelBody(colIndex = 12)
         private Long paymentAmount;
+        @ExcelBody(colIndex = 13)
         private String orderUserName;
+        @ExcelBody(colIndex = 14)
         private String deliveryType;
+        @ExcelBody(colIndex = 15)
         private String deliveryStatus;
+        @ExcelBody(colIndex = 16)
         private String deliveryCompany;
+        @ExcelBody(colIndex = 17)
         private String invoiceNo;
+        @ExcelBody(colIndex = 18)
         private String recipientName;
+        @ExcelBody(colIndex = 19)
         private String recipientUserPhone;
+        @ExcelBody(colIndex = 20)
         private String recipientAddress;
+        @ExcelBody(colIndex = 21)
         private String recipientAddressDetail;
+        @ExcelBody(colIndex = 22)
         private String requestDetail;
+        @ExcelBody(colIndex = 23)
         private String deliveryMemo;
 
         public void setDeliveryStatus(String deliveryStatus) {
