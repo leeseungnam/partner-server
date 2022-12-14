da5afb06a1eaacbafe1c8ec3a59a4a905cc6f9aa
chals <chals@wrightbrothers.kr>
2022-12-14 14:09:13 +0900

    포인트, 판매대금 주문상품목록 추가

--- src/test/java/kr/wrightbrothers/apps/OrderControllerTest.java
+++ src/test/java/kr/wrightbrothers/apps/OrderControllerTest.java
@@ -316,6 +316,8 @@ public class OrderControllerTest extends BaseControllerTests {
                                         fieldWithPath("data.productList[].returnDeliveryCompany").type(JsonFieldType.STRING).description("반품 택배 회사").optional(),
                                         fieldWithPath("data.productList[].returnDeliveryEndDay").type(JsonFieldType.STRING).description("반품완료일자").optional(),
                                         fieldWithPath("data.productList[].returnInvoiceNo").type(JsonFieldType.STRING).description("반품배송번호").optional(),
+                                        fieldWithPath("data.productList[].sspPoint").type(JsonFieldType.NUMBER).description("포인트").optional(),
+                                        fieldWithPath("data.productList[].saleAmount").type(JsonFieldType.NUMBER).description("판매대금").optional(),
                                         fieldWithPath("WBCommon.state").type(JsonFieldType.STRING).description("상태코드")
                                 )
                 ))
